838957f4e4f5959cdaac5aa375f86aa2a87ed302
Darya Nalivayko <dashanalivayko@gmail.com>
2023-06-06 16:26:48 +0300

    feat: add eslint and prettify

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,33 +1,45 @@
-import React, { FC } from 'react';
-import './button.css';
+import React, { FC } from 'react'
+import './button.css'
+import { sum } from './sum'
 
 interface IProps {
-  primary?: boolean,
-  backgroundColor?: string | null,
-  size?: string,
-  label: string,
-  onClick?: () => void,
+    primary?: boolean
+    backgroundColor?: string | null
+    size?: string
+    label: string
 }
 /**
  * Primary UI component for user interaction
  */
-export const Button: FC<IProps> = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor: backgroundColor || 'green' }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
 
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
-};
+export const Button: FC<IProps> = ({
+    primary,
+    backgroundColor,
+    size,
+    label,
+    ...props
+}) => {
+    const mode = primary
+        ? 'storybook-button--primary'
+        : 'storybook-button--secondary'
+
+    const onClick = () => {
+        sum(4, 5)
+    }
+
+    return (
+        <button
+            type="button"
+            className={[
+                'storybook-button',
+                `storybook-button--${size}`,
+                mode,
+            ].join(' ')}
+            style={{ backgroundColor: backgroundColor || 'green' }}
+            onClick={onClick}
+            {...props}
+        >
+            {label}
+        </button>
+    )
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,49 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: './index.tsx',
-	context: path.resolve(__dirname, 'src'), // базовая директория
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js', // название выходного файла
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ["babel-loader"],
-			},
-			{
-				test: /\.(js|ts)x?$/,
-				exclude: /node_modules/,
-				use: ["babel-loader"],
-
-			},
-
-		],
-	},
-	resolve: {
-		extensions: ["*", ".js", ".jsx", ".ts", ".tsx"], // позволяет не указывать расширения при импорте
-		// alias: {
-		// 	Utilities: path.resolve(__dirname, 'src/utilities/'),
-		// 	Templates: path.resolve(__dirname, 'src/templates/'),
-		//   }, ----  позволит вместо 'import Utility from '../../utilities/utility';' 
-		//            использовать 'import Utility from 'Utilities / utility';'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './index.html', // шаблон
-			filename: 'index.html', // название выходного файла
-		}),
-	],
-	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+    entry: './index.tsx',
+    context: path.resolve(__dirname, 'src'), // базовая директория
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js', // название выходного файла
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'], // позволяет не указывать расширения при импорте
+        // alias: {
+        // 	Utilities: path.resolve(__dirname, 'src/utilities/'),
+        // 	Templates: path.resolve(__dirname, 'src/templates/'),
+        //   }, ----  позволит вместо 'import Utility from '../../utilities/utility';'
+        //            использовать 'import Utility from 'Utilities / utility';'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html', // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 }
