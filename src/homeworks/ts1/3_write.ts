8396f5b8c802a347a12223582851ceaf99c74f66
Vatasi <tifu7311@yandex.ru>
2023-06-19 09:54:40 +0400

    dz

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -9,6 +9,7 @@
  * - name (строка)
  * - photo (строка, необязательно)
  *
+ 
  * Продукт (Product) содержит
  * - id (строка)
  * - name (строка)
@@ -16,7 +17,7 @@
  * - desc (строка, необязательно)
  * - createdAt (строка)
  * - oldPrice (число, необязательно)
- * - price (строка)
+ * - price (число)
  * - category (Категория)
  *
  * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
@@ -51,3 +52,101 @@
  * Принимает дату создания (строка)
  * */
 // export const createRandomOperation = (createdAt: string) => {};
+
+export type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+export type Operation = Cost | Profit;
+
+export type Cost = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost';
+};
+export type Profit = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Profit';
+};
+
+export const createRandomProduct = (createdAt: string): Product => {
+  const id = Math.floor(Math.random() * 1000000).toString();
+  const name = `Product ${id}`;
+  const photo = `https://example.com/products/${id}.jpg`;
+  const desc = `Description for ${name}`;
+  const oldPrice = Math.floor(Math.random() * 100) + 50;
+  const price = Number(`$${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 99)}`);
+  const category: Category = {
+    id: Math.floor(Math.random() * 100).toString(),
+    name: `Category ${id}`,
+    photo: `https://example.com/categories/${id}.jpg`,
+  };
+  return {
+    id,
+    name,
+    photo,
+    desc,
+    createdAt,
+    oldPrice,
+    price,
+    category,
+  };
+};
+
+/**
+ * Создает случайную операцию (Operation).
+ * Принимает дату создания (строка)
+ */
+export const createRandomOperation = (createdAt: string): Operation => {
+  const id = Math.floor(Math.random() * 1000000).toString();
+  const name = `Operation ${id}`;
+  const desc = `Description for ${name}`;
+  const amount = Math.floor(Math.random() * 1000) + 100;
+  const category: Category = {
+    id: Math.floor(Math.random() * 100).toString(),
+    name: `Category ${id}`,
+    photo: `https://example.com/categories/${id}.jpg`,
+  };
+  const type = Math.random() < 0.5 ? 'Cost' : 'Profit';
+  return type === 'Cost'
+    ? {
+        id,
+        name,
+        desc,
+        createdAt,
+        amount,
+        category,
+        type,
+      }
+    : {
+        id,
+        name,
+        desc,
+        createdAt,
+        amount,
+        category,
+        type,
+      };
+};
