8a045e7041a4bfc6c7a66fc42ab17b0596b0e949
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2023-08-31 23:55:49 +0200

    Homework1

---
+++ src/homeworks/ts1/1_base.ts
@@ -0,0 +1,79 @@
+/**
+ * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
+ * */
+export const removePlus = (string : string) => string.replace(/^\+/, '');
+export const addPlus = (string: string) => `+${string}`;
+export const removeFirstZeros = (value: string) => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+export const getBeautifulNumber = (value: string, separator = ' ') => value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+export const round = (value: number, accuracy = 2)  => {
+  const d = 10 ** accuracy;
+  return Math.round(value * d) / d;
+};
+
+const transformRegexp = /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
+
+export const getTransformFromCss = (transformCssString : string) => {
+  const data = transformCssString.match(transformRegexp);
+  
+  if (!data) return { x: 0, y: 0 };
+  return {
+    x: parseInt(data[6], 10),
+    y: parseInt(data[8], 10),
+  };
+};
+
+export const getColorContrastValue = ([red, green, blue] : number[]) =>
+  // http://www.w3.org/TR/AERT#color-contrast
+  Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+
+export const getContrastType = (contrastValue: number) => (contrastValue > 125 ? 'black' : 'white');
+
+export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
+export const longColorRegExp = /^#[0-9a-f]{6}$/i;
+
+export const checkColor = (color: string) => {
+  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) 
+    throw new Error(`invalid hex color: ${color}`);
+};
+
+export const hex2rgb = (color : string) => {
+  checkColor(color);
+  if (shortColorRegExp.test(color)) {
+    const red = parseInt(color.substring(1, 2), 16);
+    const green = parseInt(color.substring(2, 3), 16);
+    const blue = parseInt(color.substring(3, 4), 16);
+
+    return [red, green, blue];
+  }
+
+  const red = parseInt(color.substring(1, 3), 16);
+  const green = parseInt(color.substring(3, 5), 16);
+  const blue = parseInt(color.substring(5, 8), 16);
+
+  return [red, green, blue];
+};
+
+interface IResult{
+  value: number;
+  index: number;
+}
+
+// export const getNumberedArray = (arr) => arr.map((value, number) => ({ value, number }));
+export const getNumberedArray = (arr : number[]): IResult[] => arr.map((value, index) => ({value, index}))
+
+// export const toStringArray = (arr) => arr.map(({ value, number }) => `${value}_${number}`);
+export const toStringArray = (arr: IResult[]) => arr.map(({ value, index }) => `${value}_${index}`);
+
+interface ICustomer{
+  id?: number;
+  name: string;
+  age: number;
+  isSubscribed: boolean
+}
+
+export const transformCustomers = (customers: ICustomer[]) => {
+  return customers.reduce((acc, customer) => {
+    acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
+    return acc;
+  }, {} as Record<number, ICustomer>);
+};
--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -11,7 +11,14 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
- *
+ */
+export type Category = {
+    id: string;
+    name: string;
+    photo?: string
+};
+
+/**
  * Продукт (Product) содержит
  * - id (строка)
  * - name (строка)
@@ -21,9 +28,24 @@
  * - oldPrice (число, необязательно)
  * - price (число)
  * - category (Категория)
- *
+ */
+ export type Product = {
+    id: string;
+    name: string;
+    photo: string;
+    desc?: string;
+    createdAt: string;
+    oldPrice?: number;
+    price: number;
+    category: Category;
+ }
+
+/**
  * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
- *
+ */
+export type Operation = Cost | Profit
+
+/**
  * Трата (Cost) содержит
  * - id (строка)
  * - name (строка)
@@ -32,7 +54,18 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Cost')
- *
+ */
+export type Cost = {
+    id: string;
+    name: string;
+    desc?: string;
+    createdAt: string;
+    amount: number;
+    category: Category;
+    type: string;
+}
+
+/** 
  * Доход (Profit) содержит
  * - id (строка)
  * - name (строка)
@@ -41,16 +74,76 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Profit')
- * */
+ */
+export type Profit = {
+    id: string;
+    name: string;
+    desc?: string;
+    createdAt: string;
+    amount: number;
+    category: Category;
+    type: string;
+}
 
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+
+export enum Characteristic{
+    Even,
+    Odd,
+}
+
+export const isEvenNumber = (numberValue: number): boolean => (numberValue % 2 === 0);
+export const getRandomNumber = (inputNumber: number): number => (Math.round(Math.random() * inputNumber));
+export const getStringValue = (propertyName: string, randomNumber: number, characteristic?: Characteristic): string => (characteristic ? `${propertyName}${characteristic}_${randomNumber}`: `${propertyName}_${randomNumber}`);
+
+export const createProduct = (characteristic: Characteristic, createdAt: string, dateValue: Date): Product => ({
+    id: getStringValue("Id", getRandomNumber(dateValue.getFullYear()), characteristic),
+    name: getStringValue("Name", getRandomNumber(dateValue.getDate()), characteristic),
+    photo: getStringValue("Photo", getRandomNumber(dateValue.getDay()), characteristic),
+    createdAt: createdAt,
+    desc: getStringValue("Description", getRandomNumber(dateValue.getMonth()), characteristic),
+    oldPrice: getRandomNumber(dateValue.getDate()),
+    price: getRandomNumber(dateValue.getDate()),
+    category:{
+        id: getStringValue("CategoryId", getRandomNumber(dateValue.getFullYear()), characteristic),
+        name: getStringValue("CategoryName", getRandomNumber(dateValue.getDate()), characteristic),
+        photo: getStringValue("CategoryPhoto", getRandomNumber(dateValue.getDay()), characteristic),
+    } 
+})
+
+export const createRandomProduct = (createdAt: string): Product => {
+
+    const dateValue = new Date(createdAt);
+    return isEvenNumber(getRandomNumber(dateValue.getDate())) 
+        ? createProduct(Characteristic.Even, createdAt, dateValue)
+        : createProduct(Characteristic.Odd, createdAt, dateValue)
+};
+
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createOperation = (operation: string, createdAt: string, dateValue: Date): Operation => ({
+    id: getStringValue("Id", getRandomNumber(dateValue.getFullYear())),
+    name: getStringValue("Name", getRandomNumber(dateValue.getDate())),
+    desc: getStringValue("Description", getRandomNumber(dateValue.getMonth())),
+    createdAt: createdAt,
+    amount: getRandomNumber(dateValue.getDate()),
+    category: {
+        id: getStringValue("CategoryId", getRandomNumber(dateValue.getFullYear())),
+        name: getStringValue("CategoryName", getRandomNumber(dateValue.getDate())),
+        photo: getStringValue("CategoryPhoto", getRandomNumber(dateValue.getDay())),
+    },
+    type: operation,
+})
+
+export const createRandomOperation = (createdAt: string): Operation => {
+    const dateValue = new Date(createdAt);
+    return isEvenNumber(getRandomNumber(dateValue.getDate()))
+        ? (createOperation("Cost", createdAt, dateValue) as Cost)
+        : (createOperation("Profit", createdAt, dateValue) as Profit)
+};
