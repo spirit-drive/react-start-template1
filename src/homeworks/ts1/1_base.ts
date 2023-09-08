5769a5877788eab41b613069c24902fb63d4a67f
aivenkov <aleksandr.ivenkov@quadcode.com>
2023-09-08 20:55:30 +0300

    homework1

---
+++ src/homeworks/ts1/1_base.ts
@@ -0,0 +1,85 @@
+/**
+ * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
+ * */
+
+export const removePlus = (string: string): string => string.replace(/^\+/, '');
+
+export const addPlus = (string: string): string => `+${string}`;
+
+export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+
+export const getBeautifulNumber = (value: number, separator = ' '): string =>
+  value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+
+export const round = (value: number, accuracy = 2): number => {
+  const d = 10 ** accuracy;
+  return Math.round(value * d) / d;
+};
+
+const transformRegexp =
+  /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
+
+interface coords {
+  x: number;
+  y: number;
+}
+
+export const getTransformFromCss = (transformCssString: string): coords => {
+  const data: RegExpMatchArray = transformCssString.match(transformRegexp);
+  if (!data) return { x: 0, y: 0 };
+  return {
+    x: parseInt(data[6], 10),
+    y: parseInt(data[8], 10),
+  };
+};
+
+export const getColorContrastValue = ([red, green, blue]: number[]): number =>
+  // http://www.w3.org/TR/AERT#color-contrast
+  Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+
+export const getContrastType = (contrastValue: number): string => (contrastValue > 125 ? 'black' : 'white');
+
+export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
+export const longColorRegExp = /^#[0-9a-f]{6}$/i;
+
+export const checkColor = (color: string): void => {
+  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
+};
+
+export const hex2rgb = (color: string): [number, number, number] => {
+  checkColor(color);
+  if (shortColorRegExp.test(color)) {
+    const red: number = parseInt(color.substring(1, 2), 16);
+    const green: number = parseInt(color.substring(2, 3), 16);
+    const blue: number = parseInt(color.substring(3, 4), 16);
+    return [red, green, blue];
+  }
+  const red: number = parseInt(color.substring(1, 3), 16);
+  const green: number = parseInt(color.substring(3, 5), 16);
+  const blue: number = parseInt(color.substring(5, 8), 16);
+  return [red, green, blue];
+};
+
+export const getNumberedArray = (arr: number[]) =>
+  arr.map((value: number, number: number): { value: number; number: number } => ({ value, number }));
+export const toStringArray = (arr: [{ value: number; number: number }]) =>
+  arr.map(({ value, number }): string => `${value}_${number}`);
+
+interface Customer {
+  id: number;
+  name: string;
+  age: number;
+  isSubscribed: boolean;
+}
+
+interface Customers {
+  [index: number]: { name: string; age: number; isSubscribed: boolean };
+}
+
+export const transformCustomers = (customers: Customer[]) => {
+  return customers.reduce((acc: Customers, customer: Customer) => {
+    acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
+
+    return acc;
+  }, {});
+};
--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -4,6 +4,7 @@
  * Поэтому в идеале чтобы функции возвращали случайные данные, но в то же время не абракадабру.
  * В целом сделайте так, как вам будет удобно.
  * */
+import { faker } from '@faker-js/faker';
 
 /**
  * Нужно создать тип Category, он будет использоваться ниже.
@@ -11,7 +12,14 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
- *
+ **/
+type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+/**
  * Продукт (Product) содержит
  * - id (строка)
  * - name (строка)
@@ -21,9 +29,35 @@
  * - oldPrice (число, необязательно)
  * - price (число)
  * - category (Категория)
- *
+ **/
+type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+/**
  * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
  *
+ */
+type Operation = Cost | Profit;
+
+type OperationType<T extends 'Cost' | 'Profit'> = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: T;
+};
+
+/**
  * Трата (Cost) содержит
  * - id (строка)
  * - name (строка)
@@ -32,7 +66,10 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Cost')
- *
+ **/
+type Cost = OperationType<'Cost'>;
+
+/**
  * Доход (Profit) содержит
  * - id (строка)
  * - name (строка)
@@ -42,15 +79,58 @@
  * - category (Категория)
  * - type ('Profit')
  * */
+type Profit = OperationType<'Profit'>;
+
+export const createRandomCategory = (): Category => {
+  return <Category>{
+    id: faker.string.uuid(),
+    name: faker.word.noun({ length: { min: 5, max: 10 } }),
+    photo: faker.image.url(),
+  };
+};
 
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+  return {
+    id: faker.string.uuid(),
+    name: faker.word.noun({ length: { min: 5, max: 10 } }),
+    desc: faker.word.words({ count: { min: 2, max: 5 } }),
+    photo: faker.image.url(),
+    createdAt: createdAt,
+    oldPrice: faker.number.int({ min: 100, max: 200 }),
+    price: faker.number.int({ min: 100, max: 200 }),
+    category: createRandomCategory(),
+  };
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  switch (Math.round(Math.random())) {
+    case 0:
+      return {
+        id: faker.string.uuid(),
+        name: faker.word.noun({ length: { min: 5, max: 10 } }),
+        desc: faker.word.words({ count: { min: 2, max: 5 } }),
+        createdAt: createdAt,
+        category: createRandomCategory(),
+        amount: faker.number.int({ min: 10, max: 100 }),
+        type: 'Cost',
+      };
+    case 1:
+      return {
+        id: faker.string.uuid(),
+        name: faker.word.noun({ length: { min: 5, max: 10 } }),
+        desc: faker.word.words({ count: { min: 2, max: 5 } }),
+        createdAt: createdAt,
+        category: createRandomCategory(),
+        amount: faker.number.int({ min: 10, max: 100 }),
+        type: 'Profit',
+      };
+  }
+};
